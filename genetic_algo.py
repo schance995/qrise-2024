fde3712be3834a2f28a22d6b17871cfd383f8cd1
hametar0u <57603310+hametar0u@users.noreply.github.com>
2024-04-05 17:09:51 -0700

    Implemented Error Mitigation workflow
    - hardcoded Gene classes
    - hardcoded population initialization
    - hardcoded test circuit
    - The code runs without crashing

--- genetic_algo.py
+++ genetic_algo.py
@@ -3,17 +3,89 @@
 # gene = parameterized error mitigator
 # assume gene is executable, takes only circuit as parameter
 
+from abc import ABC, abstractmethod
+from functools import partial
+from mitiq.benchmarks import generate_rb_circuits
+from mitiq import rem, zne, Observable, PauliString, MeasurementResult, raw
+import cirq
+import numpy as np
+
+N_QUBITS = 2
+OBS = Observable(PauliString("Z"*N_QUBITS)) # THIS IS IN THE WRONG PLACE
+
+# TODO: for now I'm just going to hardcode the genes
+class BaseGene(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def executor(self, executable):
+        pass
+    
+class REMGene(BaseGene):
+    def __init__(self, p0=0.05, p1=0.05):
+        super().__init__()
+        self.p0 = p0
+        self.p1 = p1
+    
+    def executor(self, executable):
+        icm = rem.generate_inverse_confusion_matrix(N_QUBITS, self.p0, self.p1)
+
+        return rem.mitigate_executor(executable, inverse_confusion_matrix=icm)
+
+class ZNEGene(BaseGene):
+    def __init__(self, factory, scale_noise, num_to_avg):
+        super().__init__()
+        self.factory = factory
+        self.scale_noise = scale_noise
+        self.num_to_avg = num_to_avg
+
+    def executor(self, executable):
+        return zne.mitigate_executor(
+            executable,
+            observable=OBS,
+            factory=self.factory,
+            scale_noise=self.scale_noise,
+            num_to_average=self.num_to_avg
+        )
+
+# TODO: this is hardcoded
+def execute(circuit: cirq.Circuit, noise_level: float = 0.002, p0: float = 0.05) -> MeasurementResult:
+    """Execute a circuit with depolarizing noise of strength ``noise_level`` and readout errors ...
+    """
+    measurements = circuit[-1]
+    circuit =  circuit[:-1]
+    circuit = circuit.with_noise(cirq.depolarize(noise_level))
+    circuit.append(cirq.bit_flip(p0).on_each(circuit.all_qubits()))
+    circuit.append(measurements)
+
+    simulator = cirq.DensityMatrixSimulator()
+
+    result = simulator.run(circuit, repetitions=1000)
+    bitstrings = np.column_stack(list(result.measurements.values()))
+    return MeasurementResult(bitstrings)
+
 
 def ideal(circuit):
-    return execute(circuit)
+    return raw.execute(circuit, partial(execute, noise_level=0, p0=0), OBS) # TODO: not use this raw thing
 
-def noisy(circuit, noise):
-    return execute(circuit, noise)
+def noisy(circuit):
+    return raw.execute(circuit, execute, OBS)
 
-def mitigated(chromosome, circuit, noise):
-    result = None
-    for executable in chromosome:
-        mitigate(executable, circuit, noise) # TODO: chain the results
+def mitigated(chromosome, circuit, execute):
+    """
+    runs circuit with chromosome mitigation chain
+    exec: an executable that takes a circuit as input and outputs a QuantumResult
+    """
+
+    chain_executor = execute
+
+    for gene in chromosome:
+        chain_executor = gene.executor(chain_executor)
+
+    # result = chain_executor(circuit)
+    result = OBS.expectation(circuit, chain_executor)
+
     return result
     
 
@@ -24,18 +96,19 @@ def evaluate_fitness(chromosome, circuit):
     fitness = 0
     ideal_measurement = ideal(circuit)
     noisy_measurement = noisy(circuit)
-    mitigated_measurement = mitigated(chromosome, circuit)
+    mitigated_measurement = mitigated(chromosome, circuit, execute)
     # TODO: fitness = relative gain in mitigation
     # higher fitness = the difference between noisy and ideal > mitigated and ideal
     # ideal noise is as far away as possible
-    fitness = 1 - diff(mitigated_measurement - ideal_measurement) / diff(noisy_measurement - ideal_measurement)
+    fitness = 1 - abs(mitigated_measurement - ideal_measurement) / abs(noisy_measurement - ideal_measurement)
     return fitness
 
 
 def mutate(chromosome, p=0.5):
-    if random.uniform(0,1) < p: # some p
-        return new(chromosome) # or change its parameter
     return chromosome
+    # if np.random.uniform(0,1) < p: # some p
+    #     return new(chromosome) # or change its parameter
+    # return chromosome
 
 
 def grow_shrink(population, p=0.5):
@@ -83,5 +156,13 @@ def genetic_algorithm(circuit, generations=5, population_size=10):
     # best_index = np.argmax(fitness_scores)
     # return population[best_index], fitness_scores[best_index]
 
-circuit = None # TODO: some benchmarking circuit
+# TODO: this is hardcoded
+def initialize_population(population_size):
+    fac = zne.RichardsonFactory(scale_factors=[1, 3, 5])
+
+    # return [[REMGene(p0=0.05, p1=0.05), ZNEGene(factory=fac, scale_noise=zne.scaling.fold_global, num_to_avg=1)] for _ in range(population_size)]
+    return [[REMGene(p0=0.05, p1=0.05)] for _ in range(population_size)]
+
+
+circuit = generate_rb_circuits(2, 10)[0] # TODO: some benchmarking circuit
 print(genetic_algorithm(circuit))
